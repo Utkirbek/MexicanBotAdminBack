f884672876793fd981017676bd961dde4c3241fc
umutaliev <mutaliyevutkirbek@gmail.com>
2022-09-17 20:39:57 +0500

    some changes

--- controller/userOrderController.js
+++ controller/userOrderController.js
@@ -1,5 +1,4 @@
-const Order = require('../models/Order');
-const axios = require("axios").default;
+const Order = require("../models/Order");
 
 const Option = require("../models/Option");
 const Product = require("../models/Product");
@@ -7,33 +6,29 @@ const { Bot } = require("grammy");
 
 const addOrder = async (req, res) => {
   try {
-  
-
     const newOrder = new Order({
       ...req.body,
       user: req.body.user_id,
     });
     const order = await newOrder.save();
-    
-    
-      await order.populate("user").execPopulate();
 
-      for (let i = 0; i < order.cart.length; i++) {
-        let options = [];
-        const product = await Product.findById(order.cart[i].product_id);
+    await order.populate("user").execPopulate();
 
-        order.cart[i].product = product;
+    for (let i = 0; i < order.cart.length; i++) {
+      let options = [];
+      const product = await Product.findById(order.cart[i].product_id);
 
-        for (let j = 0; j < order.cart[i].options.length; j++) {
-          const option = await Option.findById(order.cart[i].options[j]);
+      order.cart[i].product = product;
 
-          options.push(option);
-        }
-        order.cart[i].options = options;
+      for (let j = 0; j < order.cart[i].options.length; j++) {
+        const option = await Option.findById(order.cart[i].options[j]);
+
+        options.push(option);
       }
-      sendMessageToAdmins(order);
-      res.status(201).send(order);
-   
+      order.cart[i].options = options;
+    }
+    sendMessageToAdmins(order);
+    res.status(201).send(order);
   } catch (err) {
     res.status(500).send({
       message: err.message,
@@ -64,50 +59,42 @@ const getOrderById = async (req, res) => {
 };
 
 const sendMessageToAdmins = async (order) => {
-  try{
+  try {
     const ADMINS = process.env.ADMINS;
-    
-    
-    
-    const send = async (order ,chatid)=>{
-      let product_images = []
-      let message_text = ""
-      
-      for(let i = 0 ; i < order.cart.length ; i++){
-        product_images.push(order.cart[i]?.product?.image)
-        for(let j = 0 ; j < order.cart[i]?.options?.length ; j++){
-          let  options = ""
-          options += `${order.cart[i]?.options[j]?.label}`; 
-          message_text += `Product : ${order.cart[i]?.product?.title} x ${order.cart[i].quantity} + ${options} \n`
+
+    const bot = new Bot(`${process.env.BOT_TOKEN}`);
+
+    const send = async (order, chatid, bot) => {
+      let product_images = [];
+      let message_text = "";
+
+      for (let i = 0; i < order.cart.length; i++) {
+        product_images.push(order.cart[i]?.product?.image);
+        for (let j = 0; j < order.cart[i]?.options?.length; j++) {
+          let options = "";
+          options += `${order.cart[i]?.options[j]?.label}`;
+          message_text += `Product : ${order.cart[i]?.product?.title} x ${order.cart[i].quantity} + ${options} \n`;
         }
       }
-      
-        
-const message = 
-`New Order \n
+      await bot.api.sendMessage(
+        chatid,
+        `New Order \n
 ${message_text} \n
-Total Price : ${order.total} \ncod
+Total Price : ${order.total} \n
 Address : ${order.address_name} \n
 User :  https://t.me/${order.user?.username} \n`
-      
-      const URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chatid}&text=${message}&parse_mode=HTML`;
-      await axios.get(URL)
-        
-        const Location = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendlocation?chat_id=${chatid}&latitude=${order.location.lat}&longitude=${order.location.lng}`;
-        await axios.get(Location)
-          
-      
-      
-    }
+      );
+      await bot.api.sendLocation(
+        chatid,
+        order.location.lat,
+        order.location.lng
+      );
+    };
     ADMINS.split("/").forEach(async (chatid) => {
-      console.log()
-      await send(order,chatid);
+      await send(order, chatid, bot);
     });
-  }
-  catch(err){
-
-  }
-}
+  } catch (err) {}
+};
 module.exports = {
   addOrder,
   getOrderById,
