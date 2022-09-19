cf8722a553762e1ffdcb9a283d166cd8e580b269
umutaliev <mutaliyevutkirbek@gmail.com>
2022-09-20 00:14:12 +0500

    soem chnages

--- controller/openController.js
+++ controller/openController.js
@@ -32,9 +32,29 @@ const updateOpen = async (req, res) => {
     
     const open = await Open.findById(`${process.env.OPEN_ID}`);
     if (Open) {
-      open.open_time = req.body.open_time;
+      open.monday_open_time = req.body.monday_open_time;
+      open.monday_close_time = req.body.monday_close_time;
 
-      open.close_time = req.body.close_time;
+
+      open.tuesday_open_time = req.body.tuesday_open_time;
+      open.tuesday_close_time = req.body.tuesday_close_time;
+
+      open.wednesday_open_time = req.body.wednesday_open_time;
+      open.wednesday_close_time = req.body.wednesday_close_time;
+
+
+      open.thursday_open_time = req.body.thursday_open_time;
+      open.thursday_close_time = req.body.thursday_close_time;
+
+       open.friday_open_time = req.body.friday_open_time;
+       open.friday_close_time = req.body.friday_close_time;
+
+       open.saturday_open_time = req.body.saturday_open_time;
+       open.saturday_close_time = req.body.saturday_close_time;
+
+
+        open.sunday_open_time = req.body.sunday_open_time;
+        open.sunday_close_time = req.body.sunday_close_time;
 
       await open.save();
       res.send({ message: "Open Updated Successfully!" });
--- models/Open.js
+++ models/Open.js
@@ -1,14 +1,62 @@
 const mongoose = require("mongoose");
 
 const openSchema = new mongoose.Schema({
-    open_time: {
-        type: String,
-        required: true,
-    },
-    close_time: {
-        type: String,
-        required: true,
-    },
+  monday_open_time: {
+    type: String,
+    required: true,
+  },
+  monday_close_time: {
+    type: String,
+    required: true,
+  },
+  tuesday_open_time: {
+    type: String,
+    required: true,
+  },
+  tuesday_close_time: {
+    type: String,
+    required: true,
+  },
+  wednesday_open_time: {
+    type: String,
+    required: true,
+  },
+  wednesday_close_time: {
+    type: String,
+    required: true,
+  },
+  thursday_open_time: {
+    type: String,
+    required: true,
+  },
+  thursday_close_time: {
+    type: String,
+    required: true,
+  },
+  friday_open_time: {
+    type: String,
+    required: true,
+  },
+  friday_close_time: {
+    type: String,
+    required: true,
+  },
+  saturday_open_time: {
+    type: String,
+    required: false,
+  },
+  saturday_close_time: {
+    type: String,
+    required: false,
+  },
+  sunday_open_time: {
+    type: String,
+    required: false,
+  },
+  sunday_close_time: {
+    type: String,
+    required: false,
+  },
 });
 
 const Open = mongoose.model("Open", openSchema);
